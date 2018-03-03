0543d75783395a81c7df88a266186117b100d5ea
flyerming <flyerming@163.com>
2018-03-04 01:00:23 +0800

    代码调整部分错误

---
+++ tutorial/changeip.py
@@ -0,0 +1,90 @@
+import win32ras
+import time, os
+
+class IP_Handler:
+    def Connect(self,dialname, account, passwd):
+        dial_params = (dialname, '', '', account, passwd, '')
+        return win32ras.Dial(None, None, dial_params, None)
+
+
+    def DialBroadband(self):
+        dialname = '宽带连接'  # just a name
+        account = '051916754041'
+        passwd = '320481'
+        try:
+            # handle is a pid, for disconnect or showipadrress, if connect success return 0.
+            # account is the username that your ISP supposed, passwd is the password.
+            handle, result = IP_Handler.Connect(dialname, account, passwd)
+            if result == 0:
+                print("Connection success!")
+                return handle, result
+            else:
+                print("Connection failed, wait for 5 seconds and try again...")
+                time.sleep(5)
+                IP_Handler.DialBroadband()
+        except:
+            print("Can't finish this connection, please check out.")
+            return
+
+
+    def Disconnect(self,handle):
+        if handle != None:
+            try:
+                win32ras.HangUp(handle)
+                print("Disconnection success!")
+                return "success"
+            except:
+                print("Disconnection failed, wait for 5 seconds and try again...")
+                time.sleep(5)
+                IP_Handler.Disconnect()
+        else:
+            print("Can't find the process!")
+            return
+
+
+    def Check_for_Broadband(self):
+        connections = []
+        connections = win32ras.EnumConnections()
+        if (len(connections) == 0):
+            print("The system is not running any broadband connection.")
+            return
+        else:
+            print("The system is running %d broadband connection." % len(connections))
+            return connections
+
+
+    def ShowIpAddress(self,handle):
+        print(win32ras.GetConnectStatus(handle))
+        data = os.popen("ipconfig", "r").readlines()
+        have_ppp = 0
+        ip_str = None
+        for line in data:
+            if line.find("宽带连接") >= 0:
+                have_ppp = 1
+                # if your system language is English, you should write like this:
+            # if have_ppp and line.strip().startswith("IP Address"):
+            # in othewords, replace the "IPv4 地址" to "IP Address"
+            if have_ppp and line.strip().startswith("IPv4 地址"):
+                ip_str = line.split(":")[1].strip()
+                have_ppp = 0
+                print(ip_str)
+
+    # get my ipaddress anf disconnect broadband connection.
+    def ChangeIP(self):
+        data = IP_Handler.Check_for_Broadband()
+        data = IP_Handler.Check_for_Broadband()
+        # if exist running broadband connection, disconnected it.
+        if data != None:
+            for p in data:
+                IP_Handler.ShowIpAddress(p[0])
+                if (IP_Handler.Disconnect(p[0]) == "success"):
+                    print("%s has been disconnected." % p[1])
+                time.sleep(3)
+        pid, res = IP_Handler.DialBroadband()
+        IP_Handler.ShowIpAddress(pid)
+        time.sleep(3)
+        return "IP change success"
+
+#    for i in range(1,11):
+#        test = ChangeIP()
+#        print(test)
